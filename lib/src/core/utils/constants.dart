a567cc8fd49792af47f98f018c079ae2ceb7ec63
yayanrw <yayanraw@gmail.com>
2022-03-28 13:42:21 +0700

    add constant

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,9 @@
+const String materialAppTitle = 'Flutter Clean Architecture';
+
+// API
+const String baseUrl = 'https://newsapi.org/v2';
+const String apiKey = '1bd9658980344864b8dcb14140acb7e4';
+
+// Database
+const String articlesTabName = 'articles_table';
+const String databaseName = 'app_database.db';
