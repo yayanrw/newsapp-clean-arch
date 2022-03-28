c6b77972be78c11a51d395ac9357971976b0f73a
yayanrw <yayanraw@gmail.com>
2022-03-28 13:54:08 +0700

    add app routes

---
+++ lib/src/config/routes/app_routes.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: always_specify_types
+
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static Route? onGenerateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return _materialRoute(const Scaffold());
+      default:
+        return null;
+    }
+  }
+
+  static Route<dynamic> _materialRoute(Widget view) {
+    return MaterialPageRoute(builder: (_) => view);
+  }
+}
