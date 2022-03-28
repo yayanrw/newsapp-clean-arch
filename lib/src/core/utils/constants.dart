fb4f6c24e1f61fe3c831f18f3c1641ea73d09820
yayanrw <yayanraw@gmail.com>
2022-03-28 14:13:00 +0700

    change variable

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,9 +1,9 @@
-const String materialAppTitle = 'Flutter Clean Architecture';
+const String kMaterialAppTitle = 'Flutter Clean Architecture';
 
 // API
-const String baseUrl = 'https://newsapi.org/v2';
-const String apiKey = '1bd9658980344864b8dcb14140acb7e4';
+const String kBaseUrl = 'https://newsapi.org/v2';
+const String kApiKey = '1bd9658980344864b8dcb14140acb7e4';
 
 // Database
-const String articlesTabName = 'articles_table';
-const String databaseName = 'app_database.db';
+const String kArticlesTabName = 'articles_table';
+const String kDatabaseName = 'app_database.db';
