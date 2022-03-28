c43cb08740f7b76705ac59ff140c86ffac5661ae
yayanrw <yayanraw@gmail.com>
2022-03-28 14:26:55 +0700

    add source

---
+++ lib/src/domain/entities/source.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class Source extends Equatable {
+  const Source({
+    required this.id,
+    required this.name,
+  });
+
+  final String id;
+  final String name;
+
+  @override
+  // ignore: always_specify_types
+  List<Object> get props => [id, name];
+
+  @override
+  bool get stringify => true;
+}
